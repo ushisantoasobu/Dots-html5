1457b0db1ff183a1db6e7fdb0e1848edcd3e861c
ushisantoasobu <babblemann.shunsee@gmail.com>
2014-02-09 00:49:45 +0900

    prepare

---
+++ test/0.js
@@ -0,0 +1,9 @@
+describe('0', function() {
+  it('trueが返ってくるか確認する', function() {
+    expect(dots.MainCtrl.testTestem()).to.eql(true);
+  });
+
+  it('trueが返ってくるか確認する', function() {
+    expect(dots.MainCtrl.testTestem()).to.eql(true);
+  });
+});
